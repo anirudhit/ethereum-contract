58ae68968431f68b1349c553cf8c3840c62fc7da
anirudh bellamkonda <anirudhit@outlook.com>
2019-03-26 20:44:23 -0500

    ethereum-contract
    - Added validation in submitting the form for transfering ethereum

--- web/scripts/contract.js
+++ web/scripts/contract.js
@@ -1,8 +1,27 @@
 if ( typeof web3 != 'undefined') {
     web3 = new Web3(web3.currentProvider);
-  } else {
+} else {
     web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:9545"));
-  }
+}
 
-  var version = web3.version;
-  console.log("using web3 version: " + version);
+var version = web3.version;
+console.log("using web3 version: " + version);
+
+$('#contract-form').submit(function() {
+    event.preventDefault();
+    var fromAddress = $('#fromAddress').val();
+    var toAddress = $('#toAddress').val();
+    var amount = $('#amount').val();
+    if (web3.utils.isAddress(fromAddress) != true) {
+        alert('You did not enter a correct ethereum address for the sender address.');
+        return;
+    }
+    if (web3.utils.isAddress(toAddress) != true) {
+        alert('You did not enter a correct ethereum address for the recipient address.');
+        return;
+    }
+    if (amount == 0) {
+        alert('You must send more than 0 ETH.');
+        return;
+    }
+});
